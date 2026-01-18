1a22cf6f6cef7c3e4965db1ff39f2785f03f1918
Yes_noproject <maotityamony23@mekong.edu.kh>
2026-01-18 09:51:11 +0700

    connection to postgres

--- crud-api/src/main/java/com/ms/crud_api/config/OpenApiConfiguration.java
+++ crud-api/src/main/java/com/ms/crud_api/config/OpenApiConfiguration.java
@@ -1,4 +1,5 @@
 package com.ms.crud_api.config;
+
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
@@ -7,31 +8,46 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfiguration {
+
     @Bean
     public GroupedOpenApi backendGroup() {
-        return GroupedOpenApi.builder().group("backend-api").addOpenApiCustomizer(openApi -> openApi.info(getBackendApiInfo())).packagesToScan("com.ms.crud_api.controller.backend").build();
+        return GroupedOpenApi.builder()
+                .group("backend-api")
+                .addOpenApiCustomizer(openApi -> openApi.info(getBackendApiInfo()))
+                .packagesToScan("com.ms.crud_api.controller.backend")
+                .build();
     }
 
     @Bean
     public GroupedOpenApi frontendGroup() {
-        return GroupedOpenApi.builder().group("frontend-api").addOpenApiCustomizer(openApi -> openApi.info(getFrontendApiInfo())).packagesToScan("com.ms.crud_api.controller.frontend").build();
+        return GroupedOpenApi.builder()
+                .group("frontend-api")
+                .addOpenApiCustomizer(openApi -> openApi.info(getFrontendApiInfo()))
+                .packagesToScan("com.ms.crud_api.controller.frontend")
+                .build();
     }
 
     private Info getBackendApiInfo() {
-        Contact contact = new Contact();
-        contact.setName("Mao Titaymony");
-        contact.setEmail("maotityamony789@gmail.com");
-        contact.setUrl("https://github.com/monytitya");
-
-        return new Info().title("Ecommerce API").description("Backend Ecommerce API").contact(contact).version("1.0.1");
+        return new Info()
+                .title("Ecommerce API")
+                .description("Backend Ecommerce API")
+                .contact(getContact())
+                .version("1.0.1");
     }
 
     private Info getFrontendApiInfo() {
+        return new Info()
+                .title("Ecommerce API")
+                .description("Frontend Ecommerce API")
+                .contact(getContact())
+                .version("1.0.1");
+    }
+
+    private Contact getContact() {
         Contact contact = new Contact();
         contact.setName("Mao Titaymony");
         contact.setEmail("maotityamony789@gmail.com");
         contact.setUrl("https://github.com/monytitya");
-
-        return new Info().title("Ecommerce API").description("Frontend Ecommerce API").contact(contact).version("1.0.1");
+        return contact;
     }
 }
